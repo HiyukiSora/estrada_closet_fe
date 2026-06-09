a4730a9a430f750526aecd580521fdede1f0ee11
HiyukiSora <verdanskhasfallen@gmail.com>
2026-06-09 22:46:16 +0800

    add

--- src/services/axios-instance.js
+++ src/services/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:80/estrada_closet_be/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost/estrada_closet_be/api',
     headers: {
         'Content-Type': 'application/json'
     }
